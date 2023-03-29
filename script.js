9261030ea23bcd17487114508e11b4c5d21f77c8
rafael.cunha <rafael.cunha@rfalabs.ca>
2023-03-29 12:25:55 -0300

    adding redirecto to ngrok

--- script.js
+++ script.js
@@ -1,43 +1,57 @@
 
-
-
 let UUID = null
 let qrCodeSection = document.getElementById("section")
+let listAlunos = document.getElementById("table-students")
 
 
+const ngrokLink = window.prompt("ngrok link", "link")
 
 async function run(){
     await createQrCode()
     while (true){
-        console.log(".")
         const flag = await verifyIfValidate()
-        console.log(flag)
         if(!flag){
-            console.log("qrcode") 
             await createQrCode()
         }
-        await sleep(400)
-        
+        await sleep(500)   
     }
 }
 
+
+function addStudant(name, ra, time){
+    const newElement = 
+    `
+    <tr>
+        <td class="table-name">${name}</td>
+        <td class="table-ra">${ra}</td>
+        <td class="table-hora">${time}</td>
+    </tr>
+    `
+    listAlunos.innerHTML = newElement + listAlunos.innerHTML
+}
+
+
+
 async function createQrCode(){
-    const urlBase = 'http://localhost:3000/'
-    const response = await fetch(urlBase, {mode:"cors", method:"GET"})
+    const response = await fetch(ngrokLink, {mode:"cors", method:"GET"})
     const json_response = await response.json()
     UUID = json_response.uuid
-    const urlToCall = `http://localhost:3000/assign/${UUID}`
+    const urlToCall = `${ngrokLink}/assign/${UUID}`
     qrCodeSection.innerHTML = `<p>Chamada</p>`
-    qrCodeSection.innerHTML += `<img src="https://api.qrserver.com/v1/create-qr-code/?data=${urlToCall}&amp;size=200x200" alt="${UUID}" title="${UUID}" />`
+    qrCodeSection.innerHTML += `<img src="https://api.qrserver.com/v1/create-qr-code/?data=${urlToCall}&amp;size=300x300" alt="${UUID}" title="${UUID}" />`
     qrCodeSection.innerHTML += `<a href="${urlToCall}">Link</a>`
 }
 
 
 async function verifyIfValidate(){
-    console.log("validate")
-    const urlBase = 'http://localhost:3000/'
-    const response = await fetch(`${urlBase}${UUID}`,{method:"HEAD", mode:"cors"})
-    return response.status == 200
+    const response = await fetch(`${ngrokLink}/validate/${UUID}`,{method:"GET", mode:"cors"})
+    if (response.status == 200){
+        return true
+    }else{
+        const response_json = await response.json()
+        addStudant(response_json.name, response_json.ra, response_json.time)
+        return false
+    }
 }
 
 function sleep(ms) {
